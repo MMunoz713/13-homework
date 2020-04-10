a7667693d419698ed40cfded0880c849ba22ea6e
MMunoz713 <odst.mrtn@gmail.com>
2020-04-10 12:30:53 -0500

    added git ignore file

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
+Drop DATABASE if exists burgers_db;
+
 Create DATABASE burgers_db;
 use burgers_db
 
 DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
-    devoured BOOL DEFAULT false,
-    PRIMARY KEY (id)
+    id INIT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    burger_name varchar(256),
+    devoured BOOLEAN false,
 );
